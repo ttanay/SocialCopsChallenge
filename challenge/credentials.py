423ab7cd90841ae13462725a6b110b8a2b2c7b8f
ttanay <ttanay100@gmail.com>
2018-06-21 19:57:52 +0530

    setup easier

---
+++ challenge/credentials.py
@@ -0,0 +1,3 @@
+GMAIL_ID = 'ttanay100@gmail.com'
+GMAIL_PASSWORD = '<GMAIL_PASSWORD>'
+MSG91_URL = 'http://api.msg91.com/api/sendhttp.php?sender=MSGIND&route=4&mobiles={}&authkey=<AUTH-K?EY&country=91&message=Amnesi\n\nHello! your name is {} \n\nCheers\nT. Tanay'
